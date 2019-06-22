efc6c2ca207f87813d504027f70053b9e0bc3b20
HesselBouma <hesseltjeb@gmail.com>
2019-06-22 15:17:10 +0200

    Added buttons and characters

---
+++ BPW2/Assets/scripts/DoorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject door;
+
+    [SerializeField]
+    GameObject character;
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        Destroy(door);
+        Destroy(character);
+
+    }
+
+
+}
